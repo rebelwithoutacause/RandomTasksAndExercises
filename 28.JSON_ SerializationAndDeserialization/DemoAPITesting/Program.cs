73f25f2aac821666f24dec2f86ecb132a9a2fde8
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-07-26 18:48:31 +0300

    Add files via upload

---
+++ 28.JSON_ SerializationAndDeserialization/DemoAPITesting/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace DemoAPITesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeatherForecast forecast = new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 30,
+                Summary = "Hot summer days"
+            };
+
+            string weatherInfo = JsonSerializer.Serialize(forecast);
+            Console.WriteLine("Serialized JSON:\n" + weatherInfo);
+
+            string jsonString = File.ReadAllText("C:\\Users\\User\\Desktop\\data.json");
+
+            WeatherForecast forecastFromJson = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+            Console.WriteLine("\nDeserialized Object:");
+            if (forecastFromJson != null)
+            {
+                Console.WriteLine($"Date: {forecastFromJson.Date}");
+                Console.WriteLine($"TemperatureC: {forecastFromJson.TemperatureC}");
+                Console.WriteLine($"Summary: {forecastFromJson.Summary}");
+            }
+            else
+            {
+                Console.WriteLine("Deserialization failed. Object is null.");
+            }
+        }
+    }
+}
+
---
+++ 28.JSON_ SerializationAndDeserialization/DemoAPITesting/WeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoAPITesting
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int TemperatureC { get; set; } = 30;
+
+        public string Summary { get; set; } = "Hot summer days";
+    }
+}
